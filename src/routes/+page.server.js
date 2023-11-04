417fb56d1940de3c2df75ebeece34105658d7731
Jjle1999 <1813290192@qq.com>
2023-11-04 05:39:08 +0200

    first commit

---
+++ src/routes/+page.server.js
@@ -0,0 +1,44 @@
+/** @type {import('./$types').PageServerLoad} */
+export async function load({ fetch, cookies }) {
+    let session = cookies.get('SESSION');
+    if (!session) {
+        return { logged: false };
+    }
+    let res = await fetch('https://inoc.libyana.ly/oss/logged', {
+        method: 'post',
+        credentials: 'include',
+    });
+    let res_json = await res.json();
+    if ('_csrf' in res_json) {
+        let csrf = res_json['_csrf']['token']
+        cookies.set('csrf', csrf);
+        return {
+            logged: true,
+            session: session,
+            csrf: csrf,
+        };
+    }
+    return { logged: false };
+}
+
+/** @type {import('./$types').Actions} */
+export const actions = {
+    default: async ({ fetch, cookies, request }) => {
+        const data = await request.formData();
+        const user = data.get('user');
+        const pass = data.get('pass');
+        const pass64 = Buffer.from(pass).toString('base64');
+        const url_login = `https://inoc.libyana.ly/oss/login?username=${user}&password=${pass64}`;
+        console.log(url_login);
+        let res = await fetch(url_login, {
+            method: 'post',
+            credentials:'include',
+            headers: {
+                'Accept': '*/*',
+                'Host': 'inoc.libyana.ly',
+                'Referer': 'https://inoc.libyana.ly/oss/',
+            },
+        });
+        console.log(res);
+    }
+};
---
+++ src/routes/+page.svelte
@@ -0,0 +1,21 @@
+<script>
+    export let data;
+</script>
+
+<h1>Logged: {data.logged}</h1>
+
+<form method="post">
+    <ul>
+        <li>
+            <label for="user">user</label>
+            <input type="text" name="user" id="user" />
+        </li>
+        <li>
+            <label for="pass">user</label>
+            <input type="password" name="pass" id="pass" />
+        </li>
+        <li>
+            <input type="submit" value="login">
+        </li>
+    </ul>
+</form>
