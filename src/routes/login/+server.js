acda3a83d836ebe2eeb28db69f62ffb26c11e9be
Jjle1999 <1813290192@qq.com>
2023-11-04 07:37:48 +0200

    ct

--- src/routes/login/+server.js
+++ src/routes/login/+server.js
@@ -4,7 +4,9 @@ export async function POST({ request }) {
     let { user, pass } = await request.json();
     let res = await fetch(`https://inoc.libyana.ly/oss/login?username={user}&password={pass}`, {
         method: 'POST',
-        credentials: 'include',
+        headers: {
+            'Content-Type': 'application/x-www-form-urlencoded',
+        },
     })
     return json(await res.json());
 }
