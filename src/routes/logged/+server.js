6e3a80f74d7d6663b2baa68215eaff7ffac9aedf
Jjle1999 <1813290192@qq.com>
2023-11-04 22:26:17 +0200

    session

--- src/routes/logged/+server.js
+++ src/routes/logged/+server.js
@@ -1,18 +1,12 @@
 import { json } from '@sveltejs/kit'
 import { session, csrf_name, csrf_token } from '$lib/store.js'
-import { onDestroy } from 'svelte'
-
-let session_value
-// const session_unsub = 
-session.subscribe(value => session_value=value)
-// onDestroy(session_unsub)
 
 export async function POST() {
     let url_logged = 'https://inoc.libyana.ly/oss/logged'
     let res = await fetch(url_logged, {
         method: 'GET',
         headers: {
-            'Cookie': `SESSION=${session_value}`,
+            'Cookie': `SESSION=${$session}`,
         }
     })
     let result = await res.json()
