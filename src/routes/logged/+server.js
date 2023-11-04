52aee1de599ae48568c60b0a8a55d11bac192136
Jjle1999 <1813290192@qq.com>
2023-11-04 22:22:31 +0200

    store

--- src/routes/logged/+server.js
+++ src/routes/logged/+server.js
@@ -3,8 +3,9 @@ import { session, csrf_name, csrf_token } from '$lib/store.js'
 import { onDestroy } from 'svelte'
 
 let session_value
-const session_unsub = session.subscribe(value => session_value=value)
-onDestroy(session_unsub)
+// const session_unsub = 
+session.subscribe(value => session_value=value)
+// onDestroy(session_unsub)
 
 export async function POST() {
     let url_logged = 'https://inoc.libyana.ly/oss/logged'
