94fcfe8e611a5668f6ff05c19b4723f2cb1de2a9
Jjle1999 <1813290192@qq.com>
2023-11-04 08:10:40 +0200

    logged

--- src/routes/login/+server.js
+++ src/routes/login/+server.js
@@ -1,21 +1,38 @@
 import { json } from '@sveltejs/kit';
 
-export async function POST({ cookies, request }) {
-    let { user, pass } = await request.json();
-    let url = `https://inoc.libyana.ly/oss/login?username=${user}&password=${pass}`
-    let res = await fetch(url, {
+async function login(user, pass, cookies) {
+    let url_login = `https://inoc.libyana.ly/oss/login?username=${user}&password=${pass}`
+    let res = await fetch(url_login, {
         method: 'POST',
         credentials: 'include',
         headers: {
             'Content-Type': 'application/x-www-form-urlencoded',
         },
     });
-    res.headers.getSetCookie().forEach(cookie => {
-        cookie = cookie.split(';')[0];
-        cookie = cookie.split('=');
-        cookies.set(cookie[0].trim(), cookie[1].trim())
+    let data = await res.json();
+    if (data.isSuccess === 1) {
+        res.headers.getSetCookie().forEach(cookie => {
+            cookie = cookie.split(';')[0];
+            cookie = cookie.split('=');
+            cookies.set(cookie[0].trim(), cookie[1].trim())
+        });
+    }
+    return data;
+}
+
+async function logged() {
+    let url_logged = 'https://inoc.libyana.ly/oss/logged';
+    let res = await fetch(url_logged, {
+        method: 'GET',
+        credentials: 'include',
     });
-    return json({
-        data: await res.json(),
-    });
+    return await res.json();
+}
+
+export async function POST({ cookies, request }) {
+    let { user, pass } = await request.json();
+    let data = await login(user, pass, cookies);
+    if (data.isSuccess !== 1) return json(data);
+    let log = await logged();
+    return json(log);
 }
