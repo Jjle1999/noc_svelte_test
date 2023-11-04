9db5ea4720ab2d09c9173eb7e58fd522d9ad6171
Jjle1999 <1813290192@qq.com>
2023-11-04 23:47:09 +0200

    ems get

---
+++ src/routes/api/ems/+server.js
@@ -0,0 +1,29 @@
+import { session, csrf_name, csrf_token } from '$lib/store.js'
+let session_v;
+session.subscribe((value) => (session_v = value));
+let csrf_name_v;
+csrf_name.subscribe((value) => (csrf_name_v = value));
+let csrf_token_v;
+csrf_token.subscribe((value) => (csrf_token_v = value));
+
+export async function GET() {
+    let url_logged = 'https://inoc.libyana.ly/oss/ucp/CollectAlarmSyncController/getEmsState'
+    let headers = {
+        'Cookie': `SESSION=${session_v}`,
+    }
+    headers[csrf_name_v] = csrf_token_v
+    let res = await fetch(url_logged, {
+        method: 'POST',
+        headers: headers,
+        body: JSON.stringify({
+            sortName: 'NE_NAME',
+            sortOrder: "asc",
+        })
+    })
+    let result = await res.json()
+    return json(result)
+}
+
+export async function POST() {
+
+}
