13e94e6a7d03c417ddba53f1c5e30f0e8c0e6944
Jjle1999 <1813290192@qq.com>
2023-11-04 09:48:19 +0200

    same

--- src/routes/logged/+server.js
+++ src/routes/logged/+server.js
@@ -4,11 +4,7 @@ export async function POST() {
     let url_logged = 'https://inoc.libyana.ly/oss/logged';
     let res = await fetch(url_logged, {
         method: 'GET',
-        credentials: 'include',
-        // referrer: 'https://inoc.libyana.ly/oss/',
-        referrerPolicy: 'no-referrer',
-        cache: 'no-cache',
-        mode: 'no-cors',
+        credentials: 'same-origin',
     });
     return json(await res.json());
 }
