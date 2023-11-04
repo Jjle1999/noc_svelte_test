d185e3c17e403917a4beb85a6b1707c7789c93eb
Jjle1999 <1813290192@qq.com>
2023-11-04 06:13:07 +0200

    tt

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,21 +1,27 @@
 <script>
-    export let data;
+    let logged = false;
+    let user = "admin";
+    let pass = "Fuwa2022";
+    $: pass64 = btoa(pass);
+
+    function login() {
+        let url_login = `https://inoc.libyana.ly/oss/login?username=${user}&password=${pass64}`;
+        alert(url_login);
+    }
 </script>
 
-<h1>Logged: {data.logged}</h1>
+<h1>Logged: {logged}</h1>
 
-<form method="post">
-    <ul>
-        <li>
-            <label for="user">user</label>
-            <input type="text" name="user" id="user" />
-        </li>
-        <li>
-            <label for="pass">user</label>
-            <input type="password" name="pass" id="pass" />
-        </li>
-        <li>
-            <input type="submit" value="login" />
-        </li>
-    </ul>
-</form>
+<ul>
+    <li>
+        <label for="user">user</label>
+        <input type="text" bind:value={user} />
+    </li>
+    <li>
+        <label for="pass">user</label>
+        <input type="password" bind:value={pass} />
+    </li>
+    <li>
+        <button on:click={login}>登录</button>
+    </li>
+</ul>
