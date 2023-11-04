bf21ab9da48d1d191a5693190b6da95dc089284e
Jjle1999 <1813290192@qq.com>
2023-11-04 07:34:44 +0200

    t1

--- src/routes/login/+server.js
+++ src/routes/login/+server.js
@@ -1,6 +1,10 @@
 import { json } from '@sveltejs/kit';
 
 export async function POST({ request }) {
-    let data = await request.json();
-    return json(data);
+    let { user, pass } = await request.json();
+    let res = await fetch(`https://inoc.libyana.ly/oss/login?username={user}&password={pass}`, {
+        method: 'POST',
+        credentials: 'include',
+    })
+    return json(await res.json());
 }
