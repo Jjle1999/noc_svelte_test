c0d8d042ecfd3d49dd58f1ddc3a9c00b7e8c5ea5
Jjle1999 <1813290192@qq.com>
2023-11-04 07:25:35 +0200

    okk

--- src/routes/login/+server.js
+++ src/routes/login/+server.js
@@ -3,5 +3,5 @@ import { json } from '@sveltejs/kit';
 export async function POST({ request }) {
     let data = await request.json()
     console.log(data);
-    return json('OK');
+    return json({ 'res': 'ok' });
 }
